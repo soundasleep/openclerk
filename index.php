5ef1899410894a9316ca814884f1d002df82b72f
Jevon Wright <jevon@jevon.org>
2013-04-28 13:49:33 +0000

    fixing home page pointing to missing template home.php, should be index.php
    git-svn-id: https://openclerk.googlecode.com/svn/trunk@74 b12a1112-ca08-e5dd-f792-535eff3abf3b

--- index.php
+++ index.php
@@ -5,6 +5,6 @@ require("inc/global.php");
 require("layout/templates.php");
 page_header("Home", "page_home", array('common_js' => true, 'jquery' => true));
 
-require_template("home");
+require_template("index");
 
 page_footer();
