240788caf6e4bc1da37cf58b3e6c03a2a04f14a5
Jevon Wright <jevon@jevon.org>
2013-04-28 15:03:19 +0000

    adding recent list of uncaught exceptions to admin status page
    git-svn-id: https://openclerk.googlecode.com/svn/trunk@79 b12a1112-ca08-e5dd-f792-535eff3abf3b

--- admin.php
+++ admin.php
@@ -26,5 +26,40 @@ page_header("Status", "page_admin");
 TODO
 </p>
 
+<h2>Recent Exceptions</h2>
+
+<table class="standard">
+<thead>
+	<tr>
+		<th>ID</th>
+		<th>Date</th>
+		<th>Type</th>
+		<th>Message</th>
+		<th>Source</th>
+		<th>Job ID</th>
+	</tr>
+</thead>
+<tbody>
+<?php
+	$q = db()->prepare("SELECT uncaught_exceptions.*, jobs.job_type FROM uncaught_exceptions
+		LEFT JOIN jobs ON uncaught_exceptions.job_id=jobs.id
+		ORDER BY id DESC LIMIT 20");
+	$q->execute();
+	while ($e = $q->fetch()) {
+		$path = str_replace("\\", "/", $e['filename']); ?>
+	<tr>
+		<td><?php echo number_format($e['id']); ?></td>
+		<td><?php echo recent_format_html($e['created_at']); ?></td>
+		<td><?php echo htmlspecialchars($e['class_name']); ?></td>
+		<td><?php echo htmlspecialchars($e['message']); ?></td>
+		<td><?php echo htmlspecialchars(substr($path, strrpos($path, '/') + 1) . ":" . $e['line_number']); ?></td>
+		<td><?php echo htmlspecialchars($e['job_id']); echo $e['job_type'] ? (": " . htmlspecialchars($e['job_type'])) : ""; ?></td>
+	</tr>
+	<?php }
+?>
+</tbody>
+</table>
+
+
 <?php
 page_footer();
