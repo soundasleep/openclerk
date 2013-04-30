d4f443aa317b4bdef555b4d465866a215e2bf236
Jevon Wright <jevon@jevon.org>
2013-04-30 12:19:13 +0000

    fixing visual error when trying to move graphs around hidden graphs - need to iteratively modify their page orders as well
    git-svn-id: https://openclerk.googlecode.com/svn/trunk@91 b12a1112-ca08-e5dd-f792-535eff3abf3b

--- _profile_move.php
+++ _profile_move.php
@@ -12,26 +12,38 @@ if (require_get("move_up", false)) {
 	$graph = $q->fetch();
 
 	if ($graph) {
-		// previous graph - select the one with the highest page_order under this one
-		$q = db()->prepare("SELECT graphs.* FROM graphs
-			JOIN graph_pages ON graphs.page_id=graph_pages.id
-			WHERE graph_pages.user_id=? AND graphs.page_id=? AND graphs.page_order < ?
-			ORDER BY graphs.page_order DESC LIMIT 1");
-		$q->execute(array(user_id(), $graph['page_id'], $graph['page_order']));
-		$previous = $q->fetch();
+		$limit = 0;
+		// we need to cycle through any number of hidden graphs, so that the order is preserved
+		// and it doesn't look like a graph hasn't moved when there's actually a hidden graph
+		// under it
+		while ($limit++ < 100) {
+			// previous graph - select the one with the highest page_order under this one
+			$q = db()->prepare("SELECT graphs.* FROM graphs
+				JOIN graph_pages ON graphs.page_id=graph_pages.id
+				WHERE graph_pages.user_id=? AND graphs.page_id=? AND graphs.page_order < ? AND graphs.id <> ?
+				ORDER BY graphs.page_order DESC LIMIT 1");
+			$q->execute(array(user_id(), $graph['page_id'], $graph['page_order'], $graph['id']));
+			$previous = $q->fetch();
 
-		if ($previous) {
-			// swap orders
-			$q = db()->prepare("UPDATE graphs SET page_order=? WHERE id=? LIMIT 1");
-			$q->execute(array($previous['page_order'], $graph['id']));
-			$q = db()->prepare("UPDATE graphs SET page_order=? WHERE id=? LIMIT 1");
-			$q->execute(array($graph['page_order'], $previous['id']));
+			if ($previous) {
+				// swap orders
+				$q = db()->prepare("UPDATE graphs SET page_order=? WHERE id=? LIMIT 1");
+				$q->execute(array($previous['page_order'], $graph['id']));
+				$q = db()->prepare("UPDATE graphs SET page_order=? WHERE id=? LIMIT 1");
+				$q->execute(array($graph['page_order'], $previous['id']));
+			} else {
+				// nothing to do
+			}
 
-			if (!require_get('undo', false))
-				$messages[] = "Graph moved [<a href=\"" . htmlspecialchars(url_for('profile', array('page' => $graph['page_id'], 'move_down' => $graph['id'], 'undo' => 1))) . "\">undo</a>]";
-		} else {
-			// nothing to do
+			// we need to cycle through this again
+			if (!$previous || !$previous['is_removed']) {
+				break;
+			}
 		}
+
+		if (!require_get('undo', false))
+			$messages[] = "Graph moved [<a href=\"" . htmlspecialchars(url_for('profile', array('page' => $graph['page_id'], 'move_down' => $graph['id'], 'undo' => 1))) . "\">undo</a>]";
+
 	} else {
 		throw new Exception("Could not find a graph ID " . htmlspecialchars($graph_id));
 	}
@@ -50,26 +62,39 @@ if (require_get("move_down", false)) {
 	$graph = $q->fetch();
 
 	if ($graph) {
-		// previous graph - select the one with the lowest page_order above this one
-		$q = db()->prepare("SELECT graphs.* FROM graphs
-			JOIN graph_pages ON graphs.page_id=graph_pages.id
-			WHERE graph_pages.user_id=? AND graphs.page_id=? AND graphs.page_order > ?
-			ORDER BY graphs.page_order ASC LIMIT 1");
-		$q->execute(array(user_id(), $graph['page_id'], $graph['page_order']));
-		$previous = $q->fetch();
+		$limit = 0;
+		// we need to cycle through any number of hidden graphs, so that the order is preserved
+		// and it doesn't look like a graph hasn't moved when there's actually a hidden graph
+		// under it
+		while ($limit++ < 100) {
+			// previous graph - select the one with the lowest page_order above this one
+			$q = db()->prepare("SELECT graphs.* FROM graphs
+				JOIN graph_pages ON graphs.page_id=graph_pages.id
+				WHERE graph_pages.user_id=? AND graphs.page_id=? AND graphs.page_order > ? AND graphs.id <> ?
+				ORDER BY graphs.page_order ASC LIMIT 1");
+			$q->execute(array(user_id(), $graph['page_id'], $graph['page_order'], $graph['id']));
+			$previous = $q->fetch();
 
-		if ($previous) {
-			// swap orders
-			$q = db()->prepare("UPDATE graphs SET page_order=? WHERE id=? LIMIT 1");
-			$q->execute(array($previous['page_order'], $graph['id']));
-			$q = db()->prepare("UPDATE graphs SET page_order=? WHERE id=? LIMIT 1");
-			$q->execute(array($graph['page_order'], $previous['id']));
+			if ($previous) {
+				// swap orders
+				$q = db()->prepare("UPDATE graphs SET page_order=? WHERE id=? LIMIT 1");
+				$q->execute(array($previous['page_order'], $graph['id']));
+				$q = db()->prepare("UPDATE graphs SET page_order=? WHERE id=? LIMIT 1");
+				$q->execute(array($graph['page_order'], $previous['id']));
+			} else {
+				// nothing to do
+			}
 
-			if (!require_get('undo', false))
-				$messages[] = "Graph moved [<a href=\"" . htmlspecialchars(url_for('profile', array('page' => $graph['page_id'], 'move_up' => $graph['id'], 'undo' => 1))) . "\">undo</a>]";
-		} else {
-			// nothing to do
+			// we need to cycle through this again
+			if (!$previous || !$previous['is_removed']) {
+				break;
+			}
+
 		}
+
+		if (!require_get('undo', false))
+			$messages[] = "Graph moved [<a href=\"" . htmlspecialchars(url_for('profile', array('page' => $graph['page_id'], 'move_up' => $graph['id'], 'undo' => 1))) . "\">undo</a>]";
+
 	} else {
 		throw new Exception("Could not find a graph ID " . htmlspecialchars($graph_id));
 	}
