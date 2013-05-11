ae37204a8806f4388795b8be8bd8b7f7563a4b47
Jevon Wright <jevon@jevon.org>
2013-05-11 07:27:52 +0000

    adding fix for litecoinglobal securities erraneously inserted into btct securities, and vice versa - this can happen if a user inputs the wrong API key into their account
    git-svn-id: https://openclerk.googlecode.com/svn/trunk@162 b12a1112-ca08-e5dd-f792-535eff3abf3b

--- jobs/securities_litecoinglobal.php
+++ jobs/securities_litecoinglobal.php
@@ -16,38 +16,55 @@ if (!$security) {
 	throw new JobException("Cannot find a $exchange security " . $job['arg_id'] . " for user " . $job['user_id']);
 }
 
+$content1 = false;
 $content = crypto_get_contents(crypto_wrap_url('https://www.litecoinglobal.com/api/ticker/' . urlencode($security['name'])));
-if (!$content) {
-	throw new ExternalAPIException("API returned empty data");
+if ($content === "0") {
+	// is this actually a Btct.co bond?
+	$content1 = crypto_get_contents(crypto_wrap_url('https://btct.co/api/ticker/' . urlencode($security['name'])));
+	if ($content1) {
+		// this security should never have existed (can exist if someone inserts in the wrong API key)
+		crypto_log("litecoinglobal security " . htmlspecialchars($security['name']) . " is actually a btct security: removing invalid security");
+
+		$q = db()->prepare("DELETE FROM securities_litecoinglobal WHERE id=?");
+		$q->execute(array($job['arg_id']));
+
+	} else {
+		throw new ExternalAPIException("API returned empty data 0");
+	}
 }
+if (!$content1) {
+	if (!$content) {
+		throw new ExternalAPIException("API returned empty data");
+	}
 
-// fix broken JSON
-$content = preg_replace("#<!--[^>]+-->#", "", $content);
+	// fix broken JSON
+	$content = preg_replace("#<!--[^>]+-->#", "", $content);
 
-$data = json_decode($content, true);
-if (!$data) {
-	throw new ExternalAPIException("Invalid JSON detected.");
+	$data = json_decode($content, true);
+	if (!$data) {
+		throw new ExternalAPIException("Invalid JSON detected.");
+	}
+
+	// we now have a new value
+	$balance = $data['bid'];
+
+	// disable old instances
+	$q = db()->prepare("UPDATE balances SET is_recent=0 WHERE is_recent=1 AND user_id=:user_id AND exchange=:exchange AND currency=:currency AND account_id=:account_id");
+	$q->execute(array(
+		"user_id" => $job['user_id'],
+		"account_id" => $security['id'],
+		"exchange" => $exchange,
+		"currency" => $currency,
+	));
+
+	// we have a balance; update the database
+	$q = db()->prepare("INSERT INTO balances SET user_id=:user_id, exchange=:exchange, account_id=:account_id, balance=:balance, currency=:currency, is_recent=1");
+	$q->execute(array(
+		"user_id" => $job['user_id'],
+		"account_id" => $security['id'],
+		"exchange" => $exchange,
+		"currency" => $currency,
+		"balance" => $balance,
+	));
+	crypto_log("Inserted new $exchange $currency balances id=" . db()->lastInsertId());
 }
-
-// we now have a new value
-$balance = $data['bid'];
-
-// disable old instances
-$q = db()->prepare("UPDATE balances SET is_recent=0 WHERE is_recent=1 AND user_id=:user_id AND exchange=:exchange AND currency=:currency AND account_id=:account_id");
-$q->execute(array(
-	"user_id" => $job['user_id'],
-	"account_id" => $security['id'],
-	"exchange" => $exchange,
-	"currency" => $currency,
-));
-
-// we have a balance; update the database
-$q = db()->prepare("INSERT INTO balances SET user_id=:user_id, exchange=:exchange, account_id=:account_id, balance=:balance, currency=:currency, is_recent=1");
-$q->execute(array(
-	"user_id" => $job['user_id'],
-	"account_id" => $security['id'],
-	"exchange" => $exchange,
-	"currency" => $currency,
-	"balance" => $balance,
-));
-crypto_log("Inserted new $exchange $currency balances id=" . db()->lastInsertId());
