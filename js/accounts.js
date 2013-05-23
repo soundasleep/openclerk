abcbdb598a679d8b1cb5487c000f81e173dd0994
Jevon Wright <jevon@jevon.org>
2013-05-23 03:30:55 +0000

    initial implementation of sortable account rows
    git-svn-id: https://openclerk.googlecode.com/svn/trunk@193 b12a1112-ca08-e5dd-f792-535eff3abf3b

--- js/accounts.js
+++ js/accounts.js
@@ -40,3 +40,60 @@ $(document).ready(function() {
 	});
 });
 
+/**
+ * Enable sorting.
+ */
+$(document).ready(function() {
+	var headings = $(".standard_account_list thead th");
+	headings.each(function(index, e) {
+		if (!$(e).text().trim()) {
+			// ignore button rows
+			return;
+		}
+		var temp = $("#sort_buttons_template").clone();
+
+		// set up events
+		temp.find(".sort_up").click(function(e) {
+			$(e.target).addClass("selected");
+			temp.find(".sort_down").removeClass("selected");
+			sortBy(index, true);
+		});
+		temp.find(".sort_down").click(function(e) {
+			$(e.target).addClass("selected");
+			temp.find(".sort_up").removeClass("selected");
+			sortBy(index, false);
+		});
+
+		$(e).append(temp);
+		temp.show();
+	});
+});
+
+function sortBy(index, ascending) {
+	// get the contents
+	var tbody = $(".standard_account_list tbody");
+	var rows = $(tbody).find("tr");
+	$(rows).detach();
+
+	// now sort through them
+	$(rows).each(function(i, e) {
+		var currentRows = $(tbody).find("tr");
+		var rowKey = $(e).find("td:eq(" + index + ")");
+		$(currentRows).each(function(i2, e2) {
+			var curKey = $(e2).find("td:eq(" + index + ")");
+			if (ascending && rowKey.text().trim() < curKey.text().trim()) {
+				$(e).insertBefore(e2);
+				return false;
+			} else if (!ascending && rowKey.text().trim() > curKey.text().trim()) {
+				$(e).insertBefore(e2);
+				return false;
+			}
+		});
+		if (!$(e).is(":visible")) {
+			// not anywhere; add it to the end
+			$(tbody).append(e);
+		}
+		return true;
+	});
+
+}
