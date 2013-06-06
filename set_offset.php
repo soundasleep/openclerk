e928255c8c9fd7a81ef548081361092c4661409b
Jevon Wright <jevon@jevon.org>
2013-06-06 04:39:26 +0000

    fixing user interface bug where updating an offset would not immediately update the summary value (this was originally handled by a summary job); now, when setting an offset, the most recent summary instance value is immediately updated
    git-svn-id: https://openclerk.googlecode.com/svn/trunk@230 b12a1112-ca08-e5dd-f792-535eff3abf3b

--- set_offset.php
+++ set_offset.php
@@ -17,6 +17,15 @@ foreach ($currencies as $c) {
 			continue;
 		}
 
+		// get old offset value
+		$q = db()->prepare("SELECT * FROM offsets WHERE is_recent=1 AND currency=:currency AND user_id=:user_id LIMIT 1");
+		$q->execute(array(
+			"currency" => $c,
+			"user_id" => user_id(),
+		));
+		$offset = $q->fetch();
+		$offset = $offset ? $offset['balance'] : 0;
+
 		// update old recent values
 		$q = db()->prepare("UPDATE offsets SET is_recent=0 WHERE currency=:currency AND user_id=:user_id");
 		$q->execute(array(
@@ -30,6 +39,16 @@ foreach ($currencies as $c) {
 			"currency" => $c,
 			"user_id" => user_id(),
 			"balance" => require_post($c),
+		));
+
+		// we shouldn't have to wait for an offset value to be recalculated with a summary so that it's displayed correctly;
+		// we will just update the most recent summary_instance for this currency to the correct balance (based on the previous
+		// offset value)
+		$q = db()->prepare("UPDATE summary_instances SET balance=balance+:offset WHERE user_id=:user_id AND summary_type=:summary_type AND is_recent=1");
+		$q->execute(array(
+			"offset" => require_post($c) - $offset,
+			"summary_type" => "total".$c,
+			"user_id" => user_id(),
 		));
 
 		$messages[] = "Set " . htmlspecialchars(strtoupper($c)) . " offset to " . currency_format($c, require_post($c), 8) . ".";
