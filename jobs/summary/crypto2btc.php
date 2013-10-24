6a1460a687c007ef66f174d1aef2e6704d0ffc1c
Jevon Wright <jevon@jevon.org>
2013-10-24 22:44:31 +0000

    refactoring crypto2X functionality to use get_default_currency_exchange() fixes bug in crypto2ppc where total LTC was used instead of total NMC updating graph descriptions
    git-svn-id: https://openclerk.googlecode.com/svn/trunk@423 b12a1112-ca08-e5dd-f792-535eff3abf3b

--- jobs/summary/crypto2btc.php
+++ jobs/summary/crypto2btc.php
@@ -3,86 +3,34 @@
 /**
  * Summary job: convert all cryptocurrencies to BTC.
  */
+$currency = 'btc';
 
 // BTC is kept as-is
 $q = db()->prepare("SELECT * FROM summary_instances WHERE summary_type=? AND user_id=? AND is_recent=1");
-$q->execute(array("totalbtc", $job['user_id']));
+$q->execute(array("total" . $currency, $job['user_id']));
 if ($balance = $q->fetch()) {
+	crypto_log("Initial balance (" . strtoupper($currency) . "): " . $balance['balance']);
 	$total += $balance['balance'];
 }
 
-// LTC is converted at BTC-e ticker rate sell
-$q = db()->prepare("SELECT * FROM summary_instances WHERE summary_type=? AND user_id=? AND is_recent=1");
-$q->execute(array("totalltc", $job['user_id']));
-if ($balance = $q->fetch()) {
-	$q = db()->prepare("SELECT * FROM ticker WHERE exchange=:exchange AND currency1=:currency1 AND currency2=:currency2 AND is_recent=1");
-	$q->execute(array(
-		"exchange" => "btce",
-		"currency1" => "btc",
-		"currency2" => "ltc",
-	));
-	if ($ticker = $q->fetch()) {
-		$total += $balance['balance'] * $ticker['sell'];
-	}
-}
+// convert all cryptocurrencies as per get_default_currency_exchange()
+foreach (get_all_cryptocurrencies() as $c) {
+	if ($c == $currency) continue;
 
-// NMC is converted at BTC-e ticker rate sell
-$q = db()->prepare("SELECT * FROM summary_instances WHERE summary_type=? AND user_id=? AND is_recent=1");
-$q->execute(array("totalnmc", $job['user_id']));
-if ($balance = $q->fetch()) {
-	$q = db()->prepare("SELECT * FROM ticker WHERE exchange=:exchange AND currency1=:currency1 AND currency2=:currency2 AND is_recent=1");
-	$q->execute(array(
-		"exchange" => "btce",
-		"currency1" => "btc",
-		"currency2" => "nmc",
-	));
-	if ($ticker = $q->fetch()) {
-		$total += $balance['balance'] * $ticker['sell'];
-	}
-}
-
-// FTC is converted at BTC-e ticker rate sell
-$q = db()->prepare("SELECT * FROM summary_instances WHERE summary_type=? AND user_id=? AND is_recent=1");
-$q->execute(array("totalftc", $job['user_id']));
-if ($balance = $q->fetch()) {
-	$q = db()->prepare("SELECT * FROM ticker WHERE exchange=:exchange AND currency1=:currency1 AND currency2=:currency2 AND is_recent=1");
-	$q->execute(array(
-		"exchange" => "btce",
-		"currency1" => "btc",
-		"currency2" => "ftc",
-	));
-	if ($ticker = $q->fetch()) {
-		$total += $balance['balance'] * $ticker['sell'];
-	}
-}
-
-// PPC is converted at BTC-e ticker rate sell
-$q = db()->prepare("SELECT * FROM summary_instances WHERE summary_type=? AND user_id=? AND is_recent=1");
-$q->execute(array("totalppc", $job['user_id']));
-if ($balance = $q->fetch()) {
-	$q = db()->prepare("SELECT * FROM ticker WHERE exchange=:exchange AND currency1=:currency1 AND currency2=:currency2 AND is_recent=1");
-	$q->execute(array(
-		"exchange" => "btce",
-		"currency1" => "btc",
-		"currency2" => "ppc",
-	));
-	if ($ticker = $q->fetch()) {
-		$total += $balance['balance'] * $ticker['sell'];
-	}
-}
-
-// NVC is converted at BTC-e ticker rate sell
-$q = db()->prepare("SELECT * FROM summary_instances WHERE summary_type=? AND user_id=? AND is_recent=1");
-$q->execute(array("totalnvc", $job['user_id']));
-if ($balance = $q->fetch()) {
-	$q = db()->prepare("SELECT * FROM ticker WHERE exchange=:exchange AND currency1=:currency1 AND currency2=:currency2 AND is_recent=1");
-	$q->execute(array(
-		"exchange" => "btce",
-		"currency1" => "btc",
-		"currency2" => "nvc",
-	));
-	if ($ticker = $q->fetch()) {
-		$total += $balance['balance'] * $ticker['sell'];
+	$q = db()->prepare("SELECT * FROM summary_instances WHERE summary_type=? AND user_id=? AND is_recent=1");
+	$q->execute(array("total" . $c, $job['user_id']));
+	if ($balance = $q->fetch()) {
+		$q = db()->prepare("SELECT * FROM ticker WHERE exchange=:exchange AND currency1=:currency1 AND currency2=:currency2 AND is_recent=1");
+		$q->execute(array(
+			"exchange" => get_default_currency_exchange($c),
+			"currency1" => "btc",
+			"currency2" => $c,
+		));
+		if ($ticker = $q->fetch()) {
+			$temp = $balance['balance'] * $ticker['sell'];
+			crypto_log("+ from " . strtoupper($c) . " (" . strtoupper($currency) . "): " . ($temp));
+			$total += $temp;
+		}
 	}
 }
 
