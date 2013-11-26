5ccaf6ee5f6daa73c084ca998899210790456ab3
Jevon Wright <jevon@jevon.org>
2013-11-26 03:15:53 +0000

    premium page now also uses templates
    git-svn-id: https://openclerk.googlecode.com/svn/trunk@492 b12a1112-ca08-e5dd-f792-535eff3abf3b

--- premium.php
+++ premium.php
@@ -16,19 +16,7 @@ page_header("Premium Accounts", "page_premium");
 
 ?>
 
-<h1>Support <?php echo htmlspecialchars(get_site_config('site_name')); ?> with Premium Accounts</h1>
-
-<p>
-	You can support <?php echo htmlspecialchars(get_site_config('site_name')); ?> by purchasing a
-	premium account with <?php
-		$result = array();
-		foreach (get_site_config('premium_currencies') as $currency) {
-			$result[] = get_currency_name($currency);
-		}
-		echo implode_english($result); ?> currencies. You will also get access to exclusive, premium-only functionality such as
-	vastly increased limits on the number of addresses and accounts you may track at once,
-	and advanced reporting and notification functionality. Your jobs and reports will also have higher priority over free users.
-</p>
+<?php require_template("premium"); ?>
 
 <?php
 $welcome = false;
