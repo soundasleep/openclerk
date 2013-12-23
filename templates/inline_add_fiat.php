8baaa6642e421001d4c45b562616a2d30f17ed00
Jevon Wright <jevon@jevon.org>
2013-12-23 02:56:16 +0000

    moving 'can you add support for a fiat currency' into a separate help file
    git-svn-id: https://openclerk.googlecode.com/svn/trunk@603 b12a1112-ca08-e5dd-f792-535eff3abf3b

---
+++ templates/inline_add_fiat.php
@@ -0,0 +1,22 @@
+<h2>Overview</h2>
+
+<p>
+	We want to add support for as many fiat currencies as we can to
+	<a href="http://openclerk.org" target="_blank">Openclerk</a> (the underlying open source project),
+	with priority on fiat currencies that are in highest demand.
+</p>
+
+<h2>Requesting a new fiat currency</h2>
+
+<p>
+	If you would like Openclerk to support a new fiat currency, please let us know through one of the following methods:
+</p>
+
+<?php require_template('inline_contact'); ?>
+
+<p>
+	If you would like to increase the priority of adding your preferred fiat currency to
+	Openclerk, you might want to consider <a href="<?php echo htmlspecialchars(url_for('help')); ?>">sponsoring the task</a>
+	or supporting <?php echo htmlspecialchars(get_site_config('site_name')); ?> by
+	<a href="<?php echo htmlspecialchars(url_for('premium')); ?>">purchasing a premium account</a>.
+</p>
