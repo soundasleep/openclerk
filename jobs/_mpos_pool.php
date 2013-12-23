3aff8ce1222c9f37d47b5b6191c4d8ab0288d1c9
Jevon Wright <jevon@jevon.org>
2013-12-23 13:49:45 +0000

    fixes to MPOS pool code to handle HashFaster LTC
    git-svn-id: https://openclerk.googlecode.com/svn/trunk@632 b12a1112-ca08-e5dd-f792-535eff3abf3b

--- jobs/_mpos_pool.php
+++ jobs/_mpos_pool.php
@@ -16,16 +16,32 @@ if (preg_match("#^[0-9]+{#", $contents)) {
 }
 $data = crypto_json_decode($contents, "on getuserbalance");
 if (!isset($data['getuserbalance']['data']['confirmed'])) {
-	throw new ExternalAPIException("No confirmed balance found");
+	if (!isset($data['getuserbalance']['confirmed'])) {
+		// this will also trigger if 'confirmed' is null (isset() also checks it is not null)
+		if (strpos($contents, '"confirmed":') !== false) {
+			// if 'confirmed' is actually in the response,
+			// (a bit of a hack for an isset() but also null check)
+			$include_balance = false;
+			crypto_log("Confirmed balance was null: ignoring");
+		} else {
+			throw new ExternalAPIException("No confirmed balance found");
+		}
+	} else {
+		insert_new_balance($job, $account, $exchange, $currency, $data['getuserbalance']['confirmed']);
+	}
+} else {
+	insert_new_balance($job, $account, $exchange, $currency, $data['getuserbalance']['data']['confirmed']);
 }
-
-insert_new_balance($job, $account, $exchange, $currency, $data['getuserbalance']['data']['confirmed']);
 
 // get hashrate
 $data = crypto_json_decode(crypto_get_contents(crypto_wrap_url($api_url . "action=getuserstatus&api_key=" . $account['api_key']), isset($curl_options) ? $curl_options : array()), "on getuserstatus");
 if (!isset($data['getuserstatus']['data']['hashrate'])) {
-	throw new ExternalAPIException("No hashrate found");
+	if (!isset($data['getuserstatus']['hashrate'])) {
+		throw new ExternalAPIException("No hashrate found");
+	} else {
+		insert_new_hashrate($job, $account, $exchange, $currency, $data['getuserstatus']['hashrate'] / 1000 /* assume response is in KH/s */);
+	}
+} else {
+	insert_new_hashrate($job, $account, $exchange, $currency, $data['getuserstatus']['data']['hashrate'] / 1000 /* assume response is in KH/s */);
 }
-
-insert_new_hashrate($job, $account, $exchange, $currency, $data['getuserstatus']['data']['hashrate'] / 1000 /* assume response is in KH/s */);
 
