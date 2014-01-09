2d1ddfd1d2c7c582f970f5211c4e5cc6b8ed9ec7
Jevon Wright <jevon@jevon.org>
2014-01-09 23:59:23 +0000

    fixing "Could not get reply: error:14077458:SSL routines:SSL23_GET_SERVER_HELLO:reason(1112)" error on dogechain pool
    git-svn-id: https://openclerk.googlecode.com/svn/trunk@698 b12a1112-ca08-e5dd-f792-535eff3abf3b

--- jobs/dogechainpool.php
+++ jobs/dogechainpool.php
@@ -10,4 +10,10 @@ $currency = 'dog';
 $table = "accounts_dogechainpool";
 $api_url = "https://pool.dogechain.info/index.php?page=api&";
 
+// must force SSL3: SSL2 causes an "Could not get reply: error:14077458:SSL routines:SSL23_GET_SERVER_HELLO:reason(1112)" error
+// also see http://carnivore.it/2011/10/07/error_14077458_ssl_routines_ssl23_get_server_hello_reason_1112
+$curl_options = array(
+	CURLOPT_SSLVERSION => 3,
+);
+
 require(__DIR__ . "/_mpos_pool.php");
