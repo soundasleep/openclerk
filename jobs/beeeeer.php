bbc4262f55a8b624ff300e5eb739b3b5fd48c527
Jevon Wright <jevon@jevon.org>
2014-02-17 09:02:25 +0000

    issue #67: update b(e^5)r.org mining pool API
    git-svn-id: https://openclerk.googlecode.com/svn/trunk@843 b12a1112-ca08-e5dd-f792-535eff3abf3b

--- jobs/beeeeer.php
+++ jobs/beeeeer.php
@@ -14,14 +14,19 @@ if (!$account) {
 	throw new JobException("Cannot find a $exchange account " . $job['arg_id'] . " for user " . $job['user_id']);
 }
 
-$raw = crypto_get_contents(crypto_wrap_url("http://beeeeer.org/user/" . urlencode($account['xpm_address'])));
+$raw = crypto_get_contents(crypto_wrap_url("http://beta.beeeeer.org/index.php?p=user&s=xpm&a=" . urlencode($account['xpm_address'])));
 if (!$raw) {
 	throw new ExternalAPIException("Empty response");
 }
 
 // try and parse out the current balance
 $key = "current balance:";
-$balance = (float) substr($raw, strpos($raw, $key) + strlen($key), 30);
+$balance_string = substr($raw, strpos($raw, $key) + strlen($key), 30);
+// check balance is numeric to prevent issue #67 from occuring again
+if (!preg_match("#^[0-9\.]+#i", $balance_string)) {
+	throw new ExternalAPIException("Current balance was not numeric");
+}
+$balance = (float) $balance_string;		// will ignore any following characters
 $currency = "xpm";
 
 insert_new_balance($job, $account, $exchange, $currency, $balance);
