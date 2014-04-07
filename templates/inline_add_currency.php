cb2ed7bcb433bd52fc4d5e7fd91d720bf5d26d13
Jevon Wright <jevon@jevon.org>
2014-04-07 07:03:13 +0000

    issue #92: listing SmartCoin as a currency that cannot yet be added
    git-svn-id: https://openclerk.googlecode.com/svn/trunk@975 b12a1112-ca08-e5dd-f792-535eff3abf3b

--- templates/inline_add_currency.php
+++ templates/inline_add_currency.php
@@ -15,15 +15,12 @@
 	</ul>
 </p>
 
-<!--
 <div class="tip">
 Cryptocurrencies that do <em>not</em> have a suitable explorer API yet at the time of writing:
 <ul>
-	<li><span class="currency_name_nmc">Namecoin</span> (the <a href="http://explorer.dot-bit.org/">Namecoin block explorer</a> does not display exact amounts nor address balances)</li>
-	<li><span class="currency_name_xrp">Ripple</span> (<a href="http://rippledb.com/">Ripple DB</a> does not provide balances for an address)</li>
+	<li><span class="currency_name_smc">Smartcoin</span></li>
 </ul>
 </div>
--->
 
 <p>
 	Currently <?php echo htmlspecialchars(get_site_config('site_name')); ?> supports the <?php
