2e0171bee41a5ce3499079a9f0ce50f2b71d2cea
Jevon Wright <jevon@jevon.org>
2014-04-17 08:04:33 +0000

    generate more valid HTML on tests page display all possible tests on all test results
    git-svn-id: https://openclerk.googlecode.com/svn/trunk@986 b12a1112-ca08-e5dd-f792-535eff3abf3b

--- tests/index.php
+++ tests/index.php
@@ -17,21 +17,47 @@ class AllTests extends TestSuite {
 
 		// we just load all PHP files within this directory
 		if ($handle = opendir('.')) {
-			echo "<ul style=\"padding: 10px; list-style: none;\">";
 			while (false !== ($entry = readdir($handle))) {
 				if ($entry != "." && $entry != ".." && substr(strtolower($entry), -4) == ".php" && strtolower($entry) != 'index.php') {
 					if ($only && $entry !== $only) {
 						continue;
 					}
 					$this->addFile($entry);
+				}
+			}
+			closedir($handle);
+		}
+	}
 
+	function run($reporter) {
+
+		$only = require_get("only", false);
+
+		// we just load all PHP files within this directory
+		if ($handle = opendir('.')) {
+			echo "<ul style=\"padding: 10px; list-style: none;\">";
+			echo "<li style=\"display: inline-block; margin-right: 5px;\"><a href=\"" . url_for('tests/') . "\"><b>All tests</b></a></li>\n";
+			while (false !== ($entry = readdir($handle))) {
+				if ($entry != "." && $entry != ".." && substr(strtolower($entry), -4) == ".php" && strtolower($entry) != 'index.php') {
 					echo "<li style=\"display: inline-block; margin-right: 5px;\"><a href=\"" . url_for('tests/', array('only' => $entry)) . "\">" . htmlspecialchars($entry) . "</a></li>\n";
 				}
 			}
 			echo "</ul>";
 			closedir($handle);
 		}
+
+		parent::run($reporter);
+
+		if (get_site_config('timed_sql') && is_admin()) {
+			global $global_timed_sql;
+			echo "\n<!-- SQL debug: \n " . print_r($global_timed_sql, true) . "\n-->";
+		}
+
+		if (get_site_config('timed_curl') && is_admin()) {
+			global $global_timed_curl;
+			echo "\n<!-- CURL debug: \n " . print_r($global_timed_curl, true) . "\n-->";
+		}
+
 	}
 
 }
-
