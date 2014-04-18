60f40b282342194e75cd980dfde42dd19735efa2
Jevon Wright <jevon@jevon.org>
2014-04-18 00:05:45 +0000

    issue #132: using Composer to upgrade phpmailer from 1.71 to 5.2.6+ (dev-master)
    git-svn-id: https://openclerk.googlecode.com/svn/trunk@992 b12a1112-ca08-e5dd-f792-535eff3abf3b

--- inc/phpmailer.php
+++ inc/phpmailer.php
@@ -1,41 +1,41 @@
 <?php
 
-require(__DIR__ . "/phpmailer/class.phpmailer.php");
+require(__DIR__ . "/../vendor/phpmailer/phpmailer/class.phpmailer.php");
 
 function phpmailer($to, $to_name, $subject, $message) {
-  $mail = new PHPMailer();
+	$mail = new PHPMailer();
 
-  $mail->IsSMTP();                                      // set mailer to use SMTP
-  $mail->Host = get_site_config('phpmailer_host');  // specify main and backup server
-  $mail->SMTPAuth = true;     // turn on SMTP authentication
-  $mail->Username = get_site_config('phpmailer_username');  // SMTP username
-  $mail->Password = get_site_config('phpmailer_password'); // SMTP password
+	$mail->IsSMTP();                                      // set mailer to use SMTP
+	$mail->Host = get_site_config('phpmailer_host');  // specify main and backup server
+	$mail->SMTPAuth = true;     // turn on SMTP authentication
+	$mail->Username = get_site_config('phpmailer_username');  // SMTP username
+	$mail->Password = get_site_config('phpmailer_password'); // SMTP password
 
-  $mail->From = get_site_config('phpmailer_from');
-  $mail->FromName = get_site_config('phpmailer_from_name');
-  $mail->Sender = get_site_config('phpmailer_from');
-  $mail->AddAddress($to, $to_name);
-  if (get_site_config('phpmailer_reply_to')) {
-  	$mail->AddReplyTo(get_site_config('phpmailer_reply_to'));
-  }
+	$mail->From = get_site_config('phpmailer_from');
+	$mail->FromName = get_site_config('phpmailer_from_name');
+	$mail->Sender = get_site_config('phpmailer_from');
+	$mail->AddAddress($to, $to_name);
+	if (get_site_config('phpmailer_reply_to')) {
+		$mail->AddReplyTo(get_site_config('phpmailer_reply_to'));
+	}
 
-  if (get_site_config('phpmailer_bcc', false)) {
-  	$mail->AddBCC(get_site_config('phpmailer_bcc'));
-  }
+	if (get_site_config('phpmailer_bcc', false)) {
+		$mail->AddBCC(get_site_config('phpmailer_bcc'));
+	}
 
-  $mail->Subject = $subject;
-  $mail->Body    = $message;
+	$mail->Subject = $subject;
+	$mail->Body    = $message;
 
-  // set language
-  /*
-  init_phpmailer_language();
-  global $PHPMAILER_LANG;
-  $mail->language = $PHPMAILER_LANG;
-  */
+	// set language
+	/*
+	init_phpmailer_language();
+	global $PHPMAILER_LANG;
+	$mail->language = $PHPMAILER_LANG;
+	*/
 
-  if(!$mail->Send()) {
-  	throw new MailerException("Message could not be sent: " . $mail->ErrorInfo);
-  }
+	if(!$mail->Send()) {
+		throw new MailerException("Message could not be sent: " . $mail->ErrorInfo);
+	}
 }
 
 $PHPMAILER_LANG = array(); // will load later
