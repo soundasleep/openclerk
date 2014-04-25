aaf63d39afa3047cc8c9b770272a8c805386a31f
Jevon Wright <jevon@jevon.org>
2014-04-25 06:45:15 +0000

    issue #183: enabling Market Average currency selection for summaries, total conversion tables, summary graphs
    git-svn-id: https://openclerk.googlecode.com/svn/trunk@1090 b12a1112-ca08-e5dd-f792-535eff3abf3b

--- inc/crypto.php
+++ inc/crypto.php
@@ -474,6 +474,18 @@ function get_summary_types() {
 		}
 	}
 
+	// finally, add market averages for fiats
+	foreach (get_all_fiat_currencies() as $cur) {
+		$exchange = "average";
+		$summary_types['summary_' . $cur . '_' . $exchange] = array(
+			'currency' => $cur,
+			'key' => $cur . '_' . $exchange,
+			'title' => get_currency_name($cur) . ' (converted using market average)',
+			'short_title' => get_currency_abbr($cur) . ' (market average)',
+			'exchange' => $exchange,
+		);
+	}
+
 	return $summary_types;
 
 }
@@ -540,6 +552,17 @@ function get_total_conversion_summary_types() {
 					);
 				}
 			}
+		}
+
+		// and also all average pairs for all fiats
+		foreach (get_all_fiat_currencies() as $cur) {
+			$exchange = "average";
+			$summary_types[$cur . '_' . $exchange] = array(
+				'currency' => $cur,
+				'title' => get_currency_name($cur) . ' (converted using market average)',
+				'short_title' => get_currency_abbr($cur) . ' (market average)',
+				'exchange' => $exchange,
+			);
 		}
 
 		// sort by currency order, then title
