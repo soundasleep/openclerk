0508fa7698f759627ffd38242a4544f1666efd23
Jevon Wright <jevon@jevon.org>
2014-05-21 10:45:26 +0000

    fixing spacing between 'powered by' and version number
    git-svn-id: https://openclerk.googlecode.com/svn/trunk@1194 b12a1112-ca08-e5dd-f792-535eff3abf3b

--- templates/templates_copyright.php
+++ templates/templates_copyright.php
@@ -1,4 +1,6 @@
 <?php echo htmlspecialchars(get_site_config('site_name')); ?> &copy; 2013<?php if (date('Y') != 2013) echo "-" . date('Y'); ?>,
 <?php echo t("powered by :openclerk", array(':openclerk' => '<a href="http://openclerk.org" target="_blank">openclerk.org</a>')); ?> <?php echo htmlspecialchars(get_site_config('openclerk_version')); ?>
-- <a href="<?php echo htmlspecialchars(url_for('terms')); ?>"><?php echo ht("Terms of Service"); ?></a> - <a href="<?php echo htmlspecialchars(url_for('terms#privacy')); ?>"><?php echo ht("Privacy Policy"); ?></a><br>
+<?php echo " - "; ?>
+<a href="<?php echo htmlspecialchars(url_for('terms')); ?>"><?php echo ht("Terms of Service"); ?></a>
+<?php echo " - "; ?><a href="<?php echo htmlspecialchars(url_for('terms#privacy')); ?>"><?php echo ht("Privacy Policy"); ?></a><br>
 <div class="donate"><a href="http://code.google.com/p/openclerk/wiki/Donating" target="_blank"><?php echo ht("Donate"); ?></a>: <?php echo crypto_address('btc', '17eTMdqaFRSttfBYB9chKEzHubECZPTS6p'); ?> - <?php echo crypto_address('ltc', 'LbYmauLERxK1vyqJbB9J2MNsffsYkBSuVX'); ?></div>
