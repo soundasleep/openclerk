cf994d4bd13a41e59e14c98ee3bf9c45b05a1c7b
Jevon Wright <jevon@jevon.org>
2014-05-28 01:12:33 +0000

    issue #152: updating recent changes with 0.24 changes
    git-svn-id: https://openclerk.googlecode.com/svn/trunk@1200 b12a1112-ca08-e5dd-f792-535eff3abf3b

--- templates/_supported.php
+++ templates/_supported.php
@@ -291,6 +291,12 @@ foreach ($addresses_data as $exchange => $currencies) {
 		<li id="tab_home_updates_tab" style="display:none;">
 
 			<dl>
+				<dt>21 May</dt>
+				<dd>
+				Added initial site translations into German, French, Japanese, Russian and Chinese (Simplified) languages;
+				enabled <a href="https://code.google.com/p/openclerk/wiki/HelpTranslate">contributions for improving these translations</a>.
+				</dd>
+
 				<dt>12 May</dt>
 				<dd>
 				Added <a href="<?php echo htmlspecialchars(url_for('finance_accounts')); ?>">finance accounts</a> and <a href="<?php echo htmlspecialchars(url_for('finance_categories')); ?>">finance categories</a>;
