12e0191671eb7733c217fb9780a7b6f8fe3ce5fe
Jevon Wright <jevon@jevon.org>
2014-06-25 05:00:12 +0000

    issue #236: playing around with a basic responsive design for the global site template
    git-svn-id: https://openclerk.googlecode.com/svn/trunk@1251 b12a1112-ca08-e5dd-f792-535eff3abf3b

--- layout/templates.php
+++ layout/templates.php
@@ -63,22 +63,22 @@ function page_header($page_title, $page_id = false, $options = array()) {
 
 <div id="navigation">
 <ul>
-	<li class="home"><a href="<?php echo url_for('index'); ?>"><?php echo htmlspecialchars(get_site_config('site_name')); ?></a></li>
+	<li class="home"><a href="<?php echo url_for('index'); ?>" title="<?php echo htmlspecialchars(get_site_config('site_name')); ?>"><span class="text"><?php echo htmlspecialchars(get_site_config('site_name')); ?></span></a></li>
 	<?php if (user_logged_in()) { ?>
-		<li class="profile"><a href="<?php echo url_for('profile'); ?>"><?php echo ht("Your Reports"); ?></a></li>
-		<li class="finance"><a href="<?php echo url_for('your_transactions'); ?>"><?php echo ht("Finance"); ?></a></li>
-		<li class="accounts"><a href="<?php echo url_for('wizard_currencies'); ?>"><?php echo ht("Configure Accounts"); ?></a></li>
-		<li class="user"><a href="<?php echo url_for('user'); ?>"><?php echo ht("User Profile"); ?></a></li>
-		<li class="logout"><a href="<?php echo url_for('login', array('logout' => 1)); ?>"><?php echo ht("Logout"); ?></a></li>
+		<li class="profile"><a href="<?php echo url_for('profile'); ?>" title="<?php echo ht("Your Reports"); ?>"><span class="text"><?php echo ht("Your Reports"); ?></span></a></li>
+		<li class="finance"><a href="<?php echo url_for('your_transactions'); ?>" title="<?php echo ht("Finance"); ?>"><span class="text"><?php echo ht("Finance"); ?></span></a></li>
+		<li class="accounts"><a href="<?php echo url_for('wizard_currencies'); ?>" title="<?php echo ht("Configure Accounts"); ?>"><span class="text"><?php echo ht("Configure Accounts"); ?></span></a></li>
+		<li class="user"><a href="<?php echo url_for('user'); ?>" title="<?php echo ht("User Profile"); ?>"><span class="text"><?php echo ht("User Profile"); ?></span></a></li>
+		<li class="logout"><a href="<?php echo url_for('login', array('logout' => 1)); ?>" title="<?php echo ht("Logout"); ?>"><span class="text"><?php echo ht("Logout"); ?></span></a></li>
 		<?php if (is_admin()) { ?>
-			<li class="admin"><a href="<?php echo url_for('admin'); ?>"><?php echo ht("Admin"); ?></a></li>
+			<li class="admin"><a href="<?php echo url_for('admin'); ?>" title="<?php echo ht("Admin"); ?>"><span class="text"><?php echo ht("Admin"); ?></span></a></li>
 		<?php } ?>
 	<?php } else { ?>
-		<li class="signup"><a href="<?php echo url_for('signup'); ?>"><?php echo ht("Signup"); ?></a></li>
-		<li class="login"><a href="<?php echo url_for('login'); ?>"><?php echo ht("Login"); ?></a></li>
+		<li class="signup"><a href="<?php echo url_for('signup'); ?>" title="<?php echo ht("Signup"); ?>"><span class="text"><?php echo ht("Signup"); ?></span></a></li>
+		<li class="login"><a href="<?php echo url_for('login'); ?>" title="<?php echo ht("Login"); ?>"><span class="text"><?php echo ht("Login"); ?></span></a></li>
 	<?php } ?>
-	<li class="premium"><a href="<?php echo url_for('premium'); ?>"><?php echo ht("Premium"); ?></a></li>
-	<li class="help"><a href="<?php echo url_for('help'); ?>"><?php echo ht("Help"); ?></a></li>
+	<li class="premium"><a href="<?php echo url_for('premium'); ?>" title="<?php echo ht("Premium"); ?>"><span class="text"><?php echo ht("Premium"); ?></span></a></li>
+	<li class="help"><a href="<?php echo url_for('help'); ?>" title="<?php echo ht("Help"); ?>"><span class="text"><?php echo ht("Help"); ?></span></a></li>
 </ul>
 </div>
 
--- templates/templates_head.php
+++ templates/templates_head.php
@@ -1,1 +1,2 @@
     <link rel="shortcut icon" href="<?php echo htmlspecialchars(url_for('favicon.ico')); ?>" type="image/x-icon">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
