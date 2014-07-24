1432cf04ff6a0511a2aef5c20535997778133cac
Jevon Wright <jevon@jevon.org>
2014-07-24 03:24:59 +0000

    issue #264: allow the list of votable coins to be automatically updated from TheMoneyConverter
    git-svn-id: https://openclerk.googlecode.com/svn/trunk@1285 b12a1112-ca08-e5dd-f792-535eff3abf3b

--- jobs/reported_currencies/themoneyconverter.php
+++ jobs/reported_currencies/themoneyconverter.php
@@ -2,6 +2,7 @@
 
 /**
  * TheMoneyConverter reported currencies job (#121).
+ * Also update the list of currencies that can be voted on (#264).
  */
 
 // get the feed
@@ -12,14 +13,25 @@ $xml = new SimpleXMLElement($feed);
 
 $nodes = $xml->xpath("/rss/channel/item/title");
 $currencies = array();
+$currency_votes = array();
 foreach ($nodes as $node) {
 	$pair = explode("/", strtolower((string) $node));
 	if (count($pair) == 2) {
 		$currencies[] = trim($pair[0]);
 		$currencies[] = trim($pair[1]);
 	}
-}
 
+	$description = $node->xpath("../description");
+	if (count($description)) {
+		$title = (string) $description[0];
+		// get the last text
+		$matches = false;
+		if (preg_match("/= [0-9\.]+ (.+)$/i", $title, $matches)) {
+			$currency_votes[strtoupper(trim($pair[0]))] = $matches[1];
+		}
+
+	}
+}
 
 $currencies = array_unique($currencies);
 crypto_log("Found currencies " . implode(", ", $currencies));
@@ -31,4 +43,20 @@ $q->execute(array($exchange['name']));
 foreach ($currencies as $cur) {
 	$q = db()->prepare("INSERT INTO reported_currencies SET exchange=?, currency=?");
 	$q->execute(array($exchange['name'], $cur));
+}
+
+// update the list of votable currencies
+foreach ($currency_votes as $code => $name) {
+	// don't add currencies we already support
+	if (in_array(get_currency_key($code), get_all_currencies())) {
+		continue;
+	}
+
+	$q = db()->prepare("SELECT * FROM vote_coins WHERE code=? LIMIT 1");
+	$q->execute(array($code));
+	if (!$q->fetch()) {
+		$q = db()->prepare("INSERT INTO vote_coins SET code=?, title=?");
+		$q->execute(array($code, $name));
+		crypto_log("Added new vote currency $code/$name.");
+	}
 }
