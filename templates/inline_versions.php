f3dcf8a3418788d97342812b3739ae3924ee7467
Jevon Wright <jevon@jevon.org>
2014-07-24 09:08:56 +0000

    updating locale list
    git-svn-id: https://openclerk.googlecode.com/svn/trunk@1304 b12a1112-ca08-e5dd-f792-535eff3abf3b

--- templates/inline_versions.php
+++ templates/inline_versions.php
@@ -1,7 +1,7 @@
 <h2>Version History</h2>
 
 <p>
-	Recent versions can also be viewed at the <a href="http://blog.cryptfolio.com">CryptFolio Blog</a>
+	Recent version history can also be viewed at the <a href="http://blog.cryptfolio.com">CryptFolio Blog</a>
 	or on the <a href="<?php echo htmlspecialchars(get_site_config('forum_link')); ?>" target="_blank">Bitcoin Forum</a>.
 </p>
 
