648023b98db2171468eb5ae0e2de27c2cf8268cf
Jevon Wright <jevon@jevon.org>
2014-07-24 06:56:43 +0000

    issue #182: use Namecha.in instead for Namecoin addresses
    git-svn-id: https://openclerk.googlecode.com/svn/trunk@1299 b12a1112-ca08-e5dd-f792-535eff3abf3b

--- jobs/namecoin.php
+++ jobs/namecoin.php
@@ -1,21 +1,36 @@
 <?php
 
 /**
- * Namecoin Search job (FTC).
- * The Namecoin Search (through Abe) does not provide an expressive enough API
- * for things like /address/x/balance?confirmations=6
- * So, we need to emulate this by periodically querying Explorer for the current block number,
- * and HTML parsing /address pages for transactions, and reversing any transactions within
- * a block number less than the current block number (minus some number).
- * We can go zero-confirmations for users, but for payment we need to have confirmations.
+ * Namecoin Search job.
+ * We use http://namecha.in/ directly (#182).
+ * We could extend this in the future to take away unconfirmed transactions, but it's not critical.
  */
 
-$abe_data = array(
-	'currency' => 'nmc',
-	'block_table' => 'namecoin_blocks',
-	'block_job' => 'namecoin_block',
-	'explorer_url' => get_site_config('nmc_address_url'),
-	'confirmations' => get_site_config('nmc_confirmations'),
-);
+// get the relevant address
+$q = db()->prepare("SELECT * FROM addresses WHERE user_id=? AND id=?");
+$q->execute(array($job['user_id'], $job['arg_id']));
+$address = $q->fetch();
+if (!$address) {
+	throw new JobException("Cannot find an address " . $job['arg_id'] . " for user " . $job['user_id']);
+}
 
-require(__DIR__ . "/_abe_blockchain.php");
+$html = crypto_get_contents(crypto_wrap_url(get_site_config('nmc_address_url') . urlencode($address['address'])));
+
+// look for 'current balance'
+if ($address['is_received']) {
+	$matches = false;
+	if (preg_match("#>Total received</td>.+?<td>([0-9\.]+) NMC</td>#im", $html, $matches)) {
+		$balance = $matches[1];
+		insert_new_address_balance($job, $address, $balance);
+	} else {
+		throw new ExternalAPIException("Could not find received balance on page");
+	}
+} else {
+	$matches = false;
+	if (preg_match("#>Balance: ([0-9\.]+) NMC#im", $html, $matches)) {
+		$balance = $matches[1];
+		insert_new_address_balance($job, $address, $balance);
+	} else {
+		throw new ExternalAPIException("Could not find current balance on page");
+	}
+}
--- jobs/namecoin_block.php
+++ jobs/namecoin_block.php
@@ -1,24 +1,33 @@
 <?php
 
 /**
- * Get current Namecoin block number. Used to deduct unconfirmed transactions
- * when retrieving Namecoin balances.
+ * Get current Namecoin block number.
+ * Since this isn't based off Abe, we scrape HTML instead (ergh).
  */
 
-$block = crypto_get_contents(crypto_wrap_url(get_site_config('nmc_block_url')));
-if (!is_numeric($block) || !$block) {
-	throw new ExternalAPIException("Namecoin block number was not numeric: " . htmlspecialchars($block));
+$currency = "nmc";
+$block_table = "namecoin_blocks";
+
+$html = crypto_get_contents(crypto_wrap_url(get_site_config('nmc_block_url_html')));
+
+// look for the first block number (this is dreadful)
+$matches = false;
+if (preg_match("#>([0-9]+)</a></td>[^<]+<td>([0-9]+) minute#im", $html, $matches)) {
+	$block = $matches[1];
+} else {
+	throw new ExternalAPIException("Could not find current block number on page");
 }
 
-crypto_log("Current Namecoin block number: " . number_format($block));
+crypto_log("Current $currency block number: " . number_format($block));
 
 // disable old instances
-$q = db()->prepare("UPDATE namecoin_blocks SET is_recent=0 WHERE is_recent=1");
+$q = db()->prepare("UPDATE $block_table SET is_recent=0 WHERE is_recent=1");
 $q->execute();
 
 // we have a balance; update the database
-$q = db()->prepare("INSERT INTO namecoin_blocks SET blockcount=:count,is_recent=1");
+$q = db()->prepare("INSERT INTO $block_table SET blockcount=:count,is_recent=1");
 $q->execute(array(
 	"count" => $block,
 ));
-crypto_log("Inserted new namecoin_blocks id=" . db()->lastInsertId());
+crypto_log("Inserted new $block_table id=" . db()->lastInsertId());
+
