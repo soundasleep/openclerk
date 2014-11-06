b1ffc14111a5d3dcf9fac377613432ae23ef1dbd
Jevon Wright <jevon@jevon.org>
2014-11-06 22:42:56 +0000

    updating Bitcurex API and pairs
    git-svn-id: https://openclerk.googlecode.com/svn/trunk@1483 b12a1112-ca08-e5dd-f792-535eff3abf3b

--- jobs/ticker/bitcurex.php
+++ jobs/ticker/bitcurex.php
@@ -4,42 +4,23 @@
  * Bitcurex ticker job (both PLN and EUR).
  */
 
-{
-	$exchange_name = "bitcurex";
-	$currency1 = "pln";
-	$currency2 = "btc";
+$exchange_name = "bitcurex";
+$get_exchange_pairs = get_exchange_pairs();
 
-	$rates = crypto_json_decode(crypto_get_contents(crypto_wrap_url("https://pln.bitcurex.com/data/ticker.json")));
+foreach ($get_exchange_pairs['bitcurex'] as $pair) {
+	$currency1 = $pair[0];
+	$currency2 = $pair[1];
 
-	if (!isset($rates['last'])) {
+	$rates = crypto_json_decode(crypto_get_contents(crypto_wrap_url("https://bitcurex.com/api/" . urlencode($currency1) . "/ticker.json")));
+
+	if (!isset($rates['last_tx_price'])) {
 		throw new ExternalAPIException("No $currency1/$currency2 last rate for $exchange_name");
 	}
 
 	insert_new_ticker($job, $exchange, $currency1, $currency2, array(
-		// also high, low, avg, vwap, time
-		"last_trade" => $rates['last'],
-		"bid" => $rates['sell'],
-		"ask" => $rates['buy'],
-		"volume" => $rates['vol'],
-	));
-}
-
-{
-	$exchange_name = "bitcurex";
-	$currency1 = "eur";
-	$currency2 = "btc";
-
-	$rates = crypto_json_decode(crypto_get_contents(crypto_wrap_url("https://eur.bitcurex.com/data/ticker.json")));
-
-	if (!isset($rates['last'])) {
-		throw new ExternalAPIException("No $currency1/$currency2 last rate for $exchange_name");
-	}
-
-	insert_new_ticker($job, $exchange, $currency1, $currency2, array(
-		// also high, low, avg, vwap, time
-		"last_trade" => $rates['last'],
-		"bid" => $rates['sell'],
-		"ask" => $rates['buy'],
-		"volume" => $rates['vol'],
+		"last_trade" => $rates['last_tx_price'] / 1e4,
+		"bid" => $rates['best_bid'] / 1e4,
+		"ask" => $rates['best_ask'] / 1e4,
+		"volume" => $rates['total_volume'] / 1e8,
 	));
 }
