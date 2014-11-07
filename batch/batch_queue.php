efba7505826d7dec048d04657203a8341a2cc037
Jevon Wright <jevon@jevon.org>
2014-11-07 06:52:25 +0000

    do not track reported currencies of disabled exchanges
    git-svn-id: https://openclerk.googlecode.com/svn/trunk@1516 b12a1112-ca08-e5dd-f792-535eff3abf3b

--- batch/batch_queue.php
+++ batch/batch_queue.php
@@ -159,7 +159,7 @@ $standard_jobs = array(
 	array('table' => 'accounts_hashtocoins', 'type' => 'hashtocoins', 'failure' => true),
 	array('table' => 'accounts_btclevels', 'type' => 'btclevels', 'failure' => true),
 
-	array('table' => 'exchanges', 'type' => 'reported_currencies', 'query' => ' AND track_reported_currencies=1', 'user_id' => get_site_config('system_user_id')),
+	array('table' => 'exchanges', 'type' => 'reported_currencies', 'query' => ' AND track_reported_currencies=1 AND is_disabled=0', 'user_id' => get_site_config('system_user_id')),
 
 	array('table' => 'accounts_individual_cryptostocks', 'type' => 'individual_cryptostocks', 'failure' => true),
 	array('table' => 'accounts_individual_havelock', 'type' => 'individual_havelock', 'failure' => true),
