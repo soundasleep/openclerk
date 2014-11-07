b9dd19174878878c4ea94c60581024c74e5e5857
Jevon Wright <jevon@jevon.org>
2014-11-07 03:14:11 +0000

    issue #318: update help/add_service
    git-svn-id: https://openclerk.googlecode.com/svn/trunk@1501 b12a1112-ca08-e5dd-f792-535eff3abf3b

--- templates/inline_add_service.php
+++ templates/inline_add_service.php
@@ -40,6 +40,7 @@ Exchanges/pools/services that do <em>not</em> have a suitable read-only API yet 
 <div class="column">
 <ul>
 	<li>Coins-E</li>
+	<li>Coinsortium (<a href="http://redmine.jevon.org/issues/318">#318</a>)</li>
 	<li>fast-pool.com</li>
 	<li>Flexcoin (<a href="https://bitcointalk.org/index.php?topic=57732.msg2022077#msg2022077">no API yet</a>)</li>
 	<li>hash-to-coins.com (no API &ndash; <a href="http://redmine.jevon.org/issues/282">#282</a>)</li>
