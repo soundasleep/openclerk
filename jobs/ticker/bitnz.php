35cd8df1db95b70d7903862e93ad83912b5ec3eb
Jevon Wright <jevon@jevon.org>
2014-11-21 23:19:10 +0000

    issue #328: update BitNZ ticker to use actual API
    git-svn-id: https://openclerk.googlecode.com/svn/trunk@1525 b12a1112-ca08-e5dd-f792-535eff3abf3b

--- jobs/ticker/bitnz.php
+++ jobs/ticker/bitnz.php
@@ -2,46 +2,23 @@
 
 /**
  * BitNZ ticker job.
- * BitNZ does not actually have an API, so we have to do screen scraping.
+ * Issue #328: BitNZ now has an actual ticker API that we can use
  */
 
-$bitnz = array();
-require(__DIR__ . '/../../vendor/soundasleep/html5lib-php/library/HTML5/Parser.php');
-$html = crypto_get_contents(crypto_wrap_url("https://bitnz.com/orders"));
-$dom = HTML5_Parser::parse($html);
+$exchange_name = "bitnz";
+$currency1 = 'nzd';
+$currency2 = 'btc';
 
-// now load as XML
-$xml = new SimpleXMLElement($dom->saveXML());
-$x = $xml->xpath('//div[contains(@class,"sidebar")]/.');
-$bitnz["price"] = preg_replace('#[^0-9\.]#im', '', (string) $x[0]);
+$rates = crypto_json_decode(crypto_get_contents(crypto_wrap_url("https://bitnz.com/api/0/ticker")));
 
-$buy = $xml->xpath("//*/h4[text()='Buy']/..//td");
-$bitnz["buy"] = array();
-for ($i = 0; $i < count($buy); $i += 2) {
-	$bitnz["buy"][] = array("price" => (string) $buy[$i], "btc" => (string) $buy[$i+1]);
-}
-$buy = $xml->xpath("//*/h4[text()='Sell']/..//td");
-$bitnz["sell"] = array();
-for ($i = 0; $i < count($buy); $i += 2) {
-	$bitnz["sell"][] = array("price" => (string) $buy[$i], "btc" => (string) $buy[$i+1]);
+if (!isset($rates['last'])) {
+	throw new ExternalAPIException("No $currency1/$currency2 last rate for $exchange_name");
 }
 
-$rl = array('cur1' => 'nzd', 'cur2' => 'btc');
-
-if (!isset($bitnz['price'])) {
-	throw new ExternalAPIException("No " . $rl['cur1'] . "/" . $rl['cur2'] . " rate for Bitnz");
-}
-
-if ($bitnz['price'] == 0) {
-	// don't insert in a zero balance
-	throw new ExternalAPIException("Cannot insert in a zero value for BitNZ");
-}
-
-// insert in new ticker value
-insert_new_ticker($job, $exchange, strtolower($rl['cur1']), strtolower($rl['cur2']), array(
-	"last_trade" => $bitnz['price'],
-	// BitNZ reports Buy/Sell incorrectly
-	"bid" => $bitnz['buy'][0]['price'],
-	"ask" => $bitnz['sell'][0]['price'],
-	// "volume" => $obj['volume'], - BitNZ does not provide volume data, but there must be an API somewhere?
+insert_new_ticker($job, $exchange, $currency1, $currency2, array(
+	"last_trade" => $rates['last'],
+	"bid" => $rates['bid'],
+	"ask" => $rates['ask'],
+	"volume" => $rates['volume'],
+	// also 'vwap', 'high', 'low'
 ));
