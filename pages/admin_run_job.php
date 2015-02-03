6c6171cfa8013a98a6aff3acb0b15025bf3cdac8
Jevon Wright <jevon@jevon.org>
2015-02-04 10:53:06 +1300

    Allow admin_run_jobs to self-refresh page

--- pages/admin_run_job.php
+++ pages/admin_run_job.php
@@ -9,7 +9,11 @@ require(__DIR__ . "/../layout/templates.php");
 
 define('ADMIN_RUN_JOB', true);
 
-page_header("Run Job", "page_run_job");
+$options = array();
+if (require_get("refresh", false)) {
+  $options['refresh'] = require_get("refresh");
+}
+page_header("Run Job", "page_run_job", $options);
 
 $_GET['key'] = get_site_config('automated_key');
 require(__DIR__ . "/../batch/batch_run.php");
