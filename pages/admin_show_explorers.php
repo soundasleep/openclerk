965c8d06d6ffc47656026cf600c3022ed13d1269
Jevon Wright <jevon@jevon.org>
2015-03-06 19:13:49 +1300

    Display contribute links for defining component packages
    Issue #404

--- pages/admin_show_explorers.php
+++ pages/admin_show_explorers.php
@@ -26,7 +26,7 @@ $external = get_external_apis();
 foreach ($grouped['Addresses'] as $key => $data) {
 	echo "<li><span style=\"display: inline-block; min-width: 250px;\">";
 	echo get_currency_abbr($data['currency']);
-	echo " using " . $external['Address balances'][$key];
+	echo " using " . $external['Address balances'][$key]['link'];
 	echo ":</span> ";
 	echo crypto_address($data['currency'], 'example');
 	echo "</li>";
