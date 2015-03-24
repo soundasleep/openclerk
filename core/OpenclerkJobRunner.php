2393400d6cecd2839faccf63a8f3dd573d25af70
Jevon Wright <jevon@jevon.org>
2015-03-24 15:24:30 +1300

    Mark all once-failed test jobs as permanently failed

--- core/OpenclerkJobRunner.php
+++ core/OpenclerkJobRunner.php
@@ -28,6 +28,18 @@ class OpenclerkJobRunner extends JobRunner {
       return false;
     }
 
+    // mark all once-failed test jobs as failing
+    $q = $db->prepare("SELECT * FROM jobs WHERE is_executing=0 AND is_error=0 AND is_test_job=1 AND execution_count >= ?");
+    $q->execute(array(1));
+    if ($failed = $q->fetchAll()) {
+      $logger->info("Found " . number_format(count($failed)) . " test jobs that have failed once");
+      foreach ($failed as $f) {
+        $q = $db->prepare("UPDATE jobs SET is_executed=1,is_error=1 WHERE id=?");
+        $q->execute(array($f['id']));
+        $logger->info("Marked test job " . $f['id'] . " as failed");
+      }
+    }
+
     if (require_get("job_id", false)) {
       $q = $db->prepare("SELECT * FROM jobs WHERE id=? LIMIT 1");
       $q->execute(array(require_get("job_id")));
