43f7419b2b6a888752b0721ad81b3829ec8761f3
Jevon Wright <jevon@jevon.org>
2015-03-27 11:31:27 +1300

    Fix queueing invalid job type 'ghashio'

--- core/OpenclerkJobQueuer.php
+++ core/OpenclerkJobQueuer.php
@@ -69,7 +69,6 @@ class OpenclerkJobQueuer extends JobQueuer {
       array('table' => 'accounts_havelock', 'type' => 'havelock', 'failure' => true),
       array('table' => 'securities_havelock', 'type' => 'securities_havelock', 'user_id' => get_site_config('system_user_id'), 'failure' => true),
       array('table' => 'accounts_cexio', 'type' => 'cexio', 'failure' => true),
-      array('table' => 'accounts_ghashio', 'type' => 'ghashio', 'failure' => true),
       array('table' => 'accounts_cryptotrade', 'type' => 'crypto-trade', 'failure' => true),
       array('table' => 'securities_cryptotrade', 'type' => 'securities_crypto-trade', 'user_id' => get_site_config('system_user_id'), 'failure' => true),
       array('table' => 'accounts_bitstamp', 'type' => 'bitstamp', 'failure' => true),
